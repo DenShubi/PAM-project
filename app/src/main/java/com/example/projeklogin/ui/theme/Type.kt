bfe15fd8bbce03d5d78a2e8d3fa10661f07416c5
DenShubi <denindramigurd@student.ub.ac.id>
2025-10-09 14:24:04 +0700

    Add Register Page

--- app/src/main/java/com/example/projeklogin/ui/theme/Type.kt
+++ app/src/main/java/com/example/projeklogin/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.projeklogin.ui.theme // Pastikan package name ini sesuai dengan project Anda
+package com.example.projeklogin.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
